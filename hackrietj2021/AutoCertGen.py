57ae5ff4da4136329068db6c6cb6862a17e1b4e3
imvickykumar999 <imvickykumar999@gmail.com>
2021-08-08 21:29:29 +0530

    png

--- hackrietj2021/AutoCertGen.py
+++ hackrietj2021/AutoCertGen.py
@@ -5,36 +5,44 @@ import pandas as pd
 import os
 
 df = pd.read_excel('Shortlisted Teams.xlsx')
-namelist = list(df['FULL NAME ( Team Leader )'])
+teamleader = list(df['FULL NAME ( Team Leader )'])
+member1 = list(df['Full Name ( Member 1 )'])
+member2 = list(df['Full Name ( Member 2 )'])
+member3 = list(df['Full Name ( Member 3 )'])
+namelist = [teamleader, member1, member2, member3]
+
 collegelist = list(df['College Name ( All team members must belong from same College )'])
 font = ImageFont.truetype('arial.ttf',40)
 
-for i in range(len(df)):
-    img = Image.open('Certificate.png')
-    draw = ImageDraw.Draw(img)
+for j in range(len(namelist)):
+    for i in range(len(df)):
+        img = Image.open('Certificate.png')
+        draw = ImageDraw.Draw(img)
 
-    name = namelist[i]
-    college = collegelist[i]
-    text = f'''
+        name = namelist[j][i]
+        college = collegelist[i]
+        text = f'''
 
-    This is to certify that
-    "{name}"
+        This is to certify that
+        "{name}"
 
-    student of
-    "{college}"
+        student of
+        "{college}"
 
-    has participated in HACKRIETJ-2021
-    which held on 26/6/2021 by
-    "Rajasthan Institute of Engineering and Technology, Jaipur".
-    '''
+        has participated in HACKRIETJ-2021
+        which held on 26/6/2021 by
+        "Rajasthan Institute of Engineering and Technology, Jaipur".
+        '''
 
-    draw.text(xy=(470,530),
-            text='{}'.format(text),
-            fill=(0,0,0),
-            align='center',
-            font=font)
+        print(namelist[j][i])
+        if namelist[j][i] != '-' and namelist[j][i] != float('nan'):
+            draw.text(xy=(470,530),
+                    text='{}'.format(text),
+                    fill=(0,0,0),
+                    align='center',
+                    font=font)
 
-    img.save('pictures/{}.png'.format(f'{name}'))
+            img.save('pictures/{}.png'.format(f'{name}'))
 
 
 # import smtplib
